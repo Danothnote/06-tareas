221f8c270c8f3c39354e57840c278c0d9e7b13cc
Daniel Salazar <daniel.salazar.vaca@gmail.com>
2025-04-05 12:37:02 -0500

    Manipulate Strings
    Ejercicio 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CalculateDiscount } from "./components/CalculateDiscount";
 import { UserProfile } from "./components/UserProfile";
 import { Counter } from "./components/Counter";
+import { ManipulateStrings } from "./components/ManipulateStrings";
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <CalculateDiscount />
       <UserProfile />
       <Counter />
+      <ManipulateStrings />
     </div>
   );
 };
---
+++ src/components/ManipulateStrings.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+
+export const ManipulateStrings = () => {
+  const [text, setText] = useState("");
+  const refRemove = useRef();
+
+  useEffect(() => {
+    refRemove.current.disabled = text.length === 0 ? true : false;
+  }, [text]);
+
+  const addString = () => {
+    setText(text + "A");
+  };
+
+  const removeString = () => {
+    setText(text.slice(0, -1));
+  };
+  return (
+    <div className="card">
+      <h1 className="title">
+        Ejercicio 5:
+        <br />
+        Manipulación de cadenas de texto
+      </h1>
+      <div className="form-container">
+        <h3>Cadena de entrada:</h3>
+        <p>{text}</p>
+      </div>
+      <div className="buttons-container">
+        <button className="primary-button" onClick={addString}>
+          +A
+        </button>
+        <button
+          ref={refRemove}
+          className="secondary-button"
+          onClick={removeString}
+        >
+          -A
+        </button>
+      </div>
+    </div>
+  );
+};
